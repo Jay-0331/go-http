6defb3e3514b211b732d589c7522809c37a9053a
jay vekariya <vekariya.ja@northeastern.edu>
2024-05-08 12:43:30 -0400

    binding a port

--- app/main.go
+++ app/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	server "github.com/codecrafters-io/http-server-starter-go/internal"
 	// Uncomment this block to pass the first stage
 	// "net"
@@ -10,9 +8,6 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	server := server.NewServer(4221, "0.0.0.0")
 	server.Start()
 	_ = server.Accept()
